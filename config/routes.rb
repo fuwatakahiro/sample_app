5d31673087299481dd914f61750ac164972ca0c0
Fuwatakahiro <110452794+fuwatakahiro@users.noreply.github.com>
2022-08-11 07:36:02 +0000

    [Finish] 3-1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'lists/new' => "lists#new"
+  get 'lists/index' => "lists#index"
+  get 'lists/show' => "lists#show"
+  get 'lists/edit' => "lists#edit"
+  get '/top' => "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
